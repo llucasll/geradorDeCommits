924236fabcd2b3b110d431bbeea484764b470ba3
Lucas <lcamilo@funcionalcorp.com.br>
2019-08-01 10:32:04 -0300

    README incrementado

--- gerarFragmento.php
+++ gerarFragmento.php
@@ -1,19 +1,9 @@
 <?php
 	
-	/*
-		function tamanho($arq){
-			if (!file_exists($arq))
-				return 0;
-			return filesize($arq);
-		}
-		
-		if (tamanho("repoTeste/programaQualquer.php") > 0)
-	*/
-	
 	if (!file_exists("repoTeste/programaQualquer.php") || filesize("repoTeste/programaQualquer.php")==0)
 		echo PHP_EOL . "<?php" . PHP_EOL;
 		
-	$x = (int) file_get_contents("repoTeste/indice.txt"); //file_put_contents($file, $x);
+	$x = (int) file_get_contents("repoTeste/indice.txt");
 	
 	if ($x > 0)
 	
